b17becf3a6bcd923269b5a87608d4ac27c5d8d53
amrutha98-19 <amruthasreekumar98@gmail.com>
2026-07-24 21:51:14 +0530

    area of traingle

---
+++ mathprograms/area_traingle.py
@@ -0,0 +1,7 @@
+# Program to calculate the area of a triangle
+base = float(input("Enter the base length: "))
+height = float(input("Enter the height: "))
+
+area = 0.5 * base * height
+
+print("The area of the triangle is: ",area)
